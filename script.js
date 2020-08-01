c203fdcb62769da24ce25e23fb741d0991d31b7a
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-01 14:26:26 -0400

    layout of time, input fields, and buttons as desired

--- script.js
+++ script.js
@@ -17,92 +17,80 @@ var update = setInterval( function(){
 var todayDate = new Date(); 
 var currentTime = todayDate.getHours();
 
-console.log(currentTime);
+console.log("current time: " + currentTime);
+console.log("duration: " + moment.duration(60, "minutes").asHours());
 
-console.log(moment.duration().asHours());
+for (var hour = 9; hour <18; hour++) {
+    var rowDiv = $("<div class='row'>")
+    // .attr("value", hour);
 
-// Timeblocks for standard business hours
-var hoursArray = [];
-
-for (let hour = 9; hour < 10; hour++) {
-    hoursArray.push(moment({ hour }).format('h:mm a'));
-    hoursArray.push(
-        moment({
-            hour: 1
-        }).format('h:mm a')
-    );
-}
-
-console.log(hoursArray);
-
-for (i = 0; i < 9; i++) {
-    var rowDiv = $("<div class='row'>");
-
-    var colOne = $("<div class= 'col-md-2'><p class='hourLabel'>");
-    $(".hourLabel").text(hoursArray);
-
-    var colTwo = $("<div class= 'col-md-8 justify-content-center' + 'taskInput'></div>");
-
+    var colOne = $("<div class= 'col-md-2'><p class='hourLabel'>")
+    // .attr("value", hour);
+    if (hour < 12) {
+        colOne.text([hour] + ":00 am");
+    } 
+    if (hour === 12) {
+        colOne.text([hour] + ":00 pm");
+    }
+    if (hour > 12) {
+        colOne.text([hour]-12 + ":00 pm")
+    }
+    var colTwo = $("<div class= 'col-md-8 justify-content-center' + 'taskInput'></div>")
+    // .attr("data-time", dataTime);
     var colThree = $("<div class = 'col-md-1'><p class='button'>");
     
     rowDiv.append(colOne, colTwo, colThree);
     $(".container").append(rowDiv);
-
 }
 
-var saveBtn = $("<button class='save-btn' id='save'>Save</button>");
+// Click into a timeblock, enter an event text, save button
+var saveBtn = $("<button class='save-btn' id='save'>Save</button>")
+// .attr("value", hour);
 $(".button").append(saveBtn);
-
 var toDoInput = $("<input class='todo-input' id='inputID'>");
 $(".col-md-8").append(toDoInput);
 
-// if (hour < currentTime) {
-//     taskInput.attr("style", "background-color: pink;");
-// }
-// if (hour = currentTime) {
-//     taskInput.attr("style", "background-color: #94A1B3");
-// }
-//
-
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
+// Each timeblock is color coded to indicate whether it is in the past, present, or future
+if (hour < currentTime) {
+    $(".col-md-8").attr("style", "background-color: #94A1B3;");
+}
+if (hour = currentTime) {
+    $(".col-md-8").attr("style", "background-color: #FDF7EC");
+}
+if (hour > currentTime) {
+    $(".col-md-8").attr("style", "background-color: #DDE4E0");
+}
 
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
+// var user
 
-var toDoInputField = $("#inputID");
-var saveButton = $("#save");
+// renderLastSaved();
 
-renderLastSaved();
-
-function renderLastSaved() {
-    var toDoField = localStorage.getItem("text");
+// function renderLastSaved() {
+//     var toDoField = localStorage.getItem("text");
     
-    if (!toDoField) {
-        return;
-    }
-    
-    toDoInputField.textContent = text;
-}
+//     if (!toDoField) {
+//         return;
+//     }
 
-saveButton.on("click", function () {
-    event.preventDefault();
+//     toDoInputField.textContent = text;
+// }
+// // click button to save
+// saveButton.on("click", function () {
+//     event.preventDefault();
 
-    var text = $("MAKE INDIVIDUAL IDS FOR EACH INPUT FIELD?").value;
+//     var text = $("MAKE INDIVIDUAL IDS FOR EACH INPUT FIELD?").value;
 
-    if (text ==="") {
-        return;
-    } else {
-        animate button function?
+//     if (text ==="") {
+//         return;
+//     } else {
+//         animate button function?
 
-        localStorage.setItem("text", );
-        renderLastSaved;
-    }
-});
+//         localStorage.setItem("text", );
+//         renderLastSaved;
+//     }
+// });
 
 // WHEN I refresh the page
 // THEN the saved events persist
