2828db5e788b816f73940cb807d1d2f7df529fd6
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-01 22:33:31 -0400

    making progress, current working version without full local storage and color coding commented out

--- script.js
+++ script.js
@@ -11,77 +11,110 @@ var update = setInterval( function(){
 
 }, 1000)
 
-  setInterval(update, 1000);
+setInterval(update, 1000);
+
+var userInputField = $("#dataTime")
+renderLastSaved();
+convertHour();
 
 // Current Time
 var todayDate = new Date(); 
 var currentTime = todayDate.getHours();
 
 console.log("current time: " + currentTime);
-console.log("duration: " + moment.duration(60, "minutes").asHours());
+console.log(moment('9:00', "h:mm"));
 
-for (var hour = 9; hour <18; hour++) {
+function convertHour() {
+    $('div[class="col-md-2"]').each(function() {
+        var timeNow = parseInt(currentTime.format("HHmm"));
+        console.log(timeNow);
+        var indexOfRow = parseInt(timeIndex);
+        console.log(indexOfRow);
+
+        if (timeNow < indexOfRow) {
+            $(".col-md-8").attr("style", "background-color: #94A1B3;");
+        }
+    });
+}
+
+for (var hour = 7; hour < 24; hour++) {
     var rowDiv = $("<div class='row'>")
 
+    
     var colOne = $("<div class= 'col-md-2'><p class='hourLabel'>")
-    if (hour < 12) {
+    if (hour <= 12) {
         colOne.text([hour] + ":00 am");
+        // if (hour < currentTime) {
+            
+        // } 
     } 
     if (hour === 12) {
         colOne.text([hour] + ":00 pm");
+        if (colOne.text === currentTime) {
+            $(".col-md-8").attr("style", "background-color: #FDF7EC");
+        }
     }
     if (hour > 12) {
-        colOne.text([hour]-12 + ":00 pm")
+        colOne.text([hour]-12 + ":00 pm");
+        // if (hour > currentTime) {
+        //     $(".col-md-8").attr("style", "background-color: #DDE4E0");
+        // }
     }
-    var colTwo = $("<div class= 'col-md-8 justify-content-center' + 'taskInput'></div>")
+    
+    var colTwo = $("<div class= 'col-md-8 justify-content-center'>")
+    
     var colThree = $("<div class = 'col-md-1'><p class='button'>");
     
     rowDiv.append(colOne, colTwo, colThree);
     $(".container").append(rowDiv);
 }
+    console.log(hour);
 
 // Click into a timeblock, enter an event text, save button
-var saveBtn = $("<button class='save-btn' id='save'>Save</button>")
-// .attr("value", hour);
+var saveBtn = $("<button class='save-btn' id='save'>Save</button>");
 $(".button").append(saveBtn);
-var toDoInput = $("<input class='todo-input' id='inputID'>");
+var toDoInput = $("<input class='todo-input' id='taskEntered'>").attr("type", "text");
 $(".col-md-8").append(toDoInput);
 
 // Each timeblock is color coded to indicate whether it is in the past, present, or future
-if (hour < currentTime) {
-    $(".col-md-8").attr("style", "background-color: #94A1B3;");
-}
-if (hour = currentTime) {
-    $(".col-md-8").attr("style", "background-color: #FDF7EC");
-}
-if (hour > currentTime) {
-    $(".col-md-8").attr("style", "background-color: #DDE4E0");
-}
 
 // The text for that event is saved in local storage, on page refresh the saved event persists
-var userInputField = $("#dataTime")
 
-renderLastSaved();
 
 function renderLastSaved() {
-    var text = localStorage.getItem("text");
+    $('input[type="text"]').each(function() {
+        var id = $(this).attr('id');
+        var value = localStorage.getItem(id);
+
+        $(this).val(value);
+    });
+
+    //     var text = localStorage.getItem("item");
     
-    userInputField.textContent = text;
+//     userInputField.textContent = text;
+
+// console.log(text);
 }
 
 // Click button to save
 $(".save-btn").on("click", function(event) {
     event.preventDefault();
     
-    var text = $(".todo-input").val();
+    $('input[type="text"]').each(function() {
+        var id = $(this).attr('id');
+        var value = $(this).val();
 
-    if (text === "") {
-        console.log("No entry");
-    } else {
-        localStorage.setItem("text", text);
-        renderLastSaved();
-    }
+        localStorage.setItem(id, value);
+    });
+
+    // var text = $(".todo-input").val();
+
+    // if (!text) {
+    //     console.log("No entry");
+    // } else {
+    //     localStorage.setItem("item", text);
+    //     renderLastSaved();
+    // }
 });
-
 });
 
