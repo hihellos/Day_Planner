bb1434cd8b26fa23902ae1a341dd179a287bbeb1
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-02 13:51:20 -0400

    html: hard coded schedule of hours since that is static, js: no more dynamic creation of scheudle, instead referencing ids and classes for onclick event to save and retrieve text in local storage, and for conditional formatting of colors to represent past, present, and future status of each hour at the current moment you are looking at the planner

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-// var setBoxes = localStorage.getItem("text")
-
 // Display today's date & time in jumbotron
 $("#date").text("Today is " + (moment().format('dddd, MMMM Do YYYY')));
 $("#current-time").text((moment().format('h:mm a')));
+
+var today = new Date();
+var currentTime = today.getHours();
 
 var update = setInterval( function(){
     date = moment(new Date());
@@ -14,109 +15,31 @@ var update = setInterval( function(){
 }, 1000)
 setInterval(update, 1000);
 
-var userInputField = $("#dataTime")
-renderLastSaved();
-
-// Current Time
-var todayDate = new Date(); 
-var currentTime = todayDate.getHours();
-
-console.log("current time: " + currentTime);
-console.log(parseInt(moment().format('h')));
-
-// Loop to create rows, with 3 columns for each time, task input, and save button
-// Each timeblock is color coded to indicate whether it is in the past, present, or future
-for (var hour = 7; hour < 20; hour++) {
-    var rowDiv = $("<div class='row'>")
-
-    var colOne = $("<div class= 'col-md-2'><p class='hourLabel'>")
-    var colTwo = $("<div class= 'col-md-8 justify-content-center'>").attr("value", [hour]);
-    var colThree = $("<div class = 'col-md-1'><p class='button'>");
-    
-    if (hour <= 12) {
-        colOne.text([hour] + ":00 am");
-        } 
-    if (hour === 12) {
-        colOne.text([hour] + ":00 pm");
-    }
-    if (hour > 12) {
-        colOne.text([hour]-12 + ":00 pm");
-    }
-        if ((parseInt(moment().format('h'))) <= hour + 12) {
-            $(".col-md-8").attr("style", "background-color: #E7C7C5");
-        }
-        if ((parseInt(moment().format('h'))) == hour + 12) {
-            $(".col-md-8").attr("style", "background-color: #FDF7EC");
-        }
-        if ((parseInt(moment().format('h'))) > hour + 12) {
-            $(".col-md-8").attr("style", "background-color: #DDE4E0");
-        }
-    
-    rowDiv.append(colOne, colTwo, colThree);
-    $(".container").append(rowDiv);
-}
-    console.log(hour);
-
-// Click into a timeblock, enter an event text, save button
-var saveBtn = $("<button class='save-btn' id='save'>Save</button>");
-$(".button").append(saveBtn);
-var toDoInput = $("<input class='todo-input' id='taskEntered'>").attr("type", "text");
-$(".col-md-8").append(toDoInput);
-
-// The text for that event is saved in local storage, on page refresh the saved event persists
-function renderLastSaved() {
-    $('input[type="text"]').each(function() {
-        var id = $(this).attr('id');
-        var value = localStorage.getItem(id);
-
-        $(this).val(value);
-    });
-
-// var text = localStorage.getItem("item");
-    
-// userInputField.textContent = text;
-
-// console.log(text);
-}
-
-var rowValue = $("")
-$(".col-md-2")
-
-// Click button to save
-$(".save-btn").on("click", function(event) {
-    event.preventDefault();
-    
-    $('input[type="text"]').each(function() {
-        var id = $(this).attr('id');
-        var value = $(this).val();
-
-        localStorage.setItem(id, value);
-    });
-
-    // var text = $(".todo-input").val();
-
-    // if (!text) {
-    //     console.log("No entry");
-    // } else {
-    //     localStorage.setItem("item", text);
-    //     renderLastSaved();
-    // }
-
-// Manoli class
-
 $(".saveBtn").on("click", function(){
     // set a variable to get values from text area and times
     var value = $(this).siblings(".reservation").val();
     var time = $(this).parent().attr("id");
 
-    localStorage.setItem(time,value);
-
+    localStorage.setItem(time, value);
 });
 
+$("#hr-7 .reservation").val(localStorage.getItem("hr-7"));
+$("#hr-8 .reservation").val(localStorage.getItem("hr-8"));
 $("#hr-9 .reservation").val(localStorage.getItem("hr-9"));
+$("#hr-10 .reservation").val(localStorage.getItem("hr-10"));
+$("#hr-11 .reservation").val(localStorage.getItem("hr-11"));
+$("#hr-12 .reservation").val(localStorage.getItem("hr-12"));
+$("#hr-13 .reservation").val(localStorage.getItem("hr-13"));
+$("#hr-14 .reservation").val(localStorage.getItem("hr-14"));
+$("#hr-15 .reservation").val(localStorage.getItem("hr-15"));
+$("#hr-16 .reservation").val(localStorage.getItem("hr-16"));
+$("#hr-17 .reservation").val(localStorage.getItem("hr-17"));
+$("#hr-18 .reservation").val(localStorage.getItem("hr-18"));
+$("#hr-19 .reservation").val(localStorage.getItem("hr-19"));
 
-function hourUpdate (){
+function hourUpdate(){
     var currentTime = today.getHours();
+
     $(".timeslot").each(function() {
         var blockTime = parseInt($(this).attr("id").split("-")[1]);
         if (blockTime < currentTime) {
@@ -128,11 +51,6 @@ function hourUpdate (){
         else {$(this).removeClass("past", "present").addClass("future");
         }
     });
-    
 }
-
-
-
+hourUpdate();
 });
-});
-
